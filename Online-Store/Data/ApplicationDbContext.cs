170cde02090dfac85752ca5cebb708d03f5392fb
Dimka122 <kassindima1@gmail.com>
2025-07-03 10:17:53 +0300

    создаем Модель Контролер и DbContext

---
+++ Online-Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Store.Models.Entities;
+
+namespace Online_Store.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Здесь можно настроить отношения между таблицами
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
