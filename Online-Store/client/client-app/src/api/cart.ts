1d7b46d29c05abc036de8b856e1a4f16bc80be37
Dimka122 <kassindima1@gmail.com>
2025-07-31 12:56:18 +0300

    /.../

--- Online-Store/client/client-app/src/api/cart.ts
+++
@@ -1,6 +0,0 @@
-import { apiClient } from './client'; // Или import { api } from './client', если выбрали Вариант 2
-
-export const addToCart = async (productId: string, quantity: number) => {
-  const response = await apiClient.post('/cart', { productId, quantity }); // Используйте api или apiClient в зависимости от выбранного варианта
-  return response.data;
-};
--- Online-Store/client/client-app/src/components/Pagination.tsx
+++
@@ -1,7 +0,0 @@
-interface PaginationProps {
-  currentPage: number;
-}
-
-export const Pagination = ({ currentPage }: PaginationProps) => {
-  return <div>Пагинация (страница {currentPage})</div>;
-};
